61d49cada942887ce8cd075cb884dc0fae18b183
prakash8876 <prakash8876@gmail.com>
2023-07-21 01:35:24 +0530

    python: variables and data-types

---
+++ python/04_var_and_datatypes.py
@@ -0,0 +1,40 @@
+'''Date: 21/7/23'''
+
+# variables and data-types in python
+# Integer, FLoating, String, Boolean, None are data-types
+
+a = "prakash"  # string literal
+aa = 'praakash' # string literal
+aaa = '''praaakash''' # string literal
+b = 345         # integer number
+c = 34.14       # float
+d = True        # boolean
+e = False       # boolean
+f = None        # None is nothing to declare
+
+
+# a is string data-type
+# b is integer data-type
+# c is float data-type
+
+
+# printing variables
+print("Value of a: ", a)
+print("Value of aa: ", aa)
+print("Value of aaa: ", aaa)
+print("Value of b: ", b)
+print("Value of c: ", c)
+print("Value of d: ", d)
+print("Value of e: ", e)
+print("Value of f: ", f)
+
+
+# printing types of variables
+print(type(a))
+print(type(aa))
+print(type(aaa))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
