8558d7bd1824532dcf9e9c1b4a7666f5737f5e41
prakash8876 <prakash8876@gmail.com>
2023-07-21 01:44:23 +0530

    python: operators

---
+++ python/05_operators.py
@@ -0,0 +1,34 @@
+a = 12
+b = 23
+
+# Arithmetic operators
+print("a + b ", a + b)
+print("a - b ", a - b)
+print("a * b ", a * b)
+print("a / b ", a / b)
+
+# assignment operators
+a = 34
+a += 2
+print(a)
+
+# 33/3 value should return 11, instead it return floating value as 11.0
+
+# comparison operators
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
+print(a == b)
+print(a != b)
+
+# logical operators
+# and
+# or
+# not
+
+bool1 = True
+bool2 = False
+print("Value of bool1 and bool2 is", (bool1 and bool2))
+print("Value of bool1 or bool2 is", (bool1 or bool2))
+print("Value of not bool2 is", (not bool2))
